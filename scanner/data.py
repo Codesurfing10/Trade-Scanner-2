c8e56f720ec7247dd9fce9746114c6cc731ab9b9
Codesurfing10 <130518622+Codesurfing10@users.noreply.github.com>
2026-04-20 22:59:28 -0700

    Expand DEFAULT_SYMBOLS for staples, defense, space, quantum, materials, commodities/metals.

--- scanner/data.py
+++ scanner/data.py
@@ -1,7 +1,39 @@
+# Existing Symbols
 DEFAULT_SYMBOLS = [
-    # Existing symbols...
-    'RKLB', 'SPCE', 'ASTS', 'PL', 'IRDM', 'LMT', 'NOC', 'BA', 'RTX', 'AVAV', 'ITA', 'ARKX', 'UFO',
-    'IONQ', 'RGTI', 'QBTS', 'SMCI', 'ARM', 'ASML', 'TSM', 'LIN', 'APD', 'ECL', 'SHW', 'DD', 'DOW',
-    'FCX', 'NEM', 'AEM', 'GOLD', 'AA', 'MP', 'XLB', 'GDX', 'GDXJ', 'COPX', 'LIT', 'GLD', 'SLV',
-    'PPLT', 'PALL', 'DBC', 'USO', 'UNG', 'CORN', 'WEAT', 'SOYB'
+    # Existing symbols here
+    'AAPL',
+    'GOOG',
+    'TSLA',
+    'AMZN',
+    # ... other existing symbols
 ]
+
+# Consumer Staples
+DEFAULT_SYMBOLS.extend([
+    'PG', 'KO', 'PEP', 'MDLZ', 'CL', 'KMB', 'GIS', 'KHC', 'CLX', 'PM', 'MO'
+])
+
+# Industrials/Defense
+DEFAULT_SYMBOLS.extend([
+    'LMT', 'NOC', 'RTX', 'GD', 'HII', 'LHX', 'BA'
+])
+
+# Space
+DEFAULT_SYMBOLS.extend([
+    'IRDM'
+])
+
+# Quantum/Compute Proxy
+DEFAULT_SYMBOLS.extend([
+    'IBM'
+])
+
+# Materials/Metals/Mining
+DEFAULT_SYMBOLS.extend([
+    'LIN', 'APD', 'SHW', 'ECL', 'FCX'
+])
+
+# Commodities & Metals Exposures (ETFs)
+DEFAULT_SYMBOLS.extend([
+    'GLD', 'SLV', 'PPLT', 'DBA', 'USO', 'UNG', 'XLE', 'XME', 'COPX'
+])
